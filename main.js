52d125cbbecc46b7802ed44b0fbac5683d6196f3
AlexanderStepanov <alex.stdp@gmail.com>
2018-10-16 12:04:45 +0300

    First commit. Tasks 1 - 4 must be rewritten, because they are trash. Descriptions must be added to the tasks 4 - 7.

---
+++ main.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    
+    function includeTasks (arrayOfTasksUrl) {
+        for (let i = 0; i <= arrayOfTasksUrl.length - 1; i++) {
+            var script = document.createElement('script');
+            script.src = arrayOfTasksUrl[i];
+            document.getElementsByTagName('body')[0].appendChild(script);    
+        }   
+    }
+    
+    function createTaskDescription () {
+        document.getElementById("task_description").innerHTML = taskDescription[this.id];
+    }
+    
+    function createTaskBtnHandlers (taskDescriptionButtons) {
+        for (let i = 0; i <= taskDescriptionButtons.length - 1; i++) {
+            taskDescriptionButtons[i].onclick = createTaskDescription;
+        }
+    }
+    
+    includeTasks(arrayOfTasksUrl);
+    createTaskBtnHandlers(taskDescriptionButtons);
+    
+}
